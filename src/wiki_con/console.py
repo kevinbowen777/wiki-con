1628c8dc7985d027ad4933c19cb6afb55d4e9330
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-30 13:12:37 -0700

    Create initial script

---
+++ src/wiki_con/console.py
@@ -0,0 +1,11 @@
+import click
+
+
+from .import __version__
+
+
+@click.command()
+@click.version_option(version=__version__)
+def main():
+    """The wiki_con Python project."""
+    click.echo("Hello, world!")
