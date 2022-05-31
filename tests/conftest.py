799574722083cb6e109a0c6acaec304d595bdeca
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-30 19:54:16 -0700

    Move mock_requests_get() to conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+
+
+@pytest.fixture
+def mock_requests_get(mocker):
+    mock.return_value.__enter__.return_value.json.return_value = {
+        "title": "Lorem Ipsum",
+        "extract": "Lorem ipsum dolor sit amet",
+    }
+    return mock
--- tests/test_console.py
+++ tests/test_console.py
@@ -11,13 +11,8 @@ def runner():
 
 
 @pytest.fixture
-def mock_requests_get(mocker):
-    mock = mocker.patch("requests.get")
-    mock.return_value.__enter__.return_value.json.return_value = {
-        "title": "Lorem Ipsum",
-        "extract": "Lorem ipsum dolor sit amet",
-    }
-    return mock
+def mock_wikipedia_random_page(mocker):
+    return mocker.patch("wiki_con.wikipedia.random_page")
 
 
 def test_main_succeeds(runner, mock_requests_get):
