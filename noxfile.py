c3fa118355c22e686bb242ac1b769cb7c51f62a0
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-31 10:21:36 -0700

    Add python 3.8.13 to nox sessions

--- noxfile.py
+++ noxfile.py
@@ -4,14 +4,14 @@ import nox
 locations = "src", "tests", "noxfile.py"
 
 
-@nox.session(python=["3.10", "3.9"])
+@nox.session(python=["3.10", "3.9", "3.8"])
 def tests(session):
     args = session.posargs or ["--cov", "-m", "not e2e"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
 
 
-@nox.session(python=["3.10", "3.9"])
+@nox.session(python=["3.10", "3.9", "3.8"])
 def lint(session):
     args = session.posargs or locations
     session.install("flake8")
